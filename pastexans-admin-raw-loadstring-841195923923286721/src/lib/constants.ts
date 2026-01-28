f832a492d59606023996adf501d9f7be0cb001ec
AREXANS <89494133+AREXANS@users.noreply.github.com>
2026-01-28 12:11:19 +0700

    Wipe all

--- pastexans-admin-raw-loadstring-841195923923286721/src/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-export const ADMIN_EMAILS = ["arexanss@gmail.com", "arextoreff@gmail.com"];
-export const LOGO_URL = "https://files.catbox.moe/raona5.jpg";
-
-export const DEFAULT_COPY_EXPIRY_OPTIONS: ('permanent' | '1h' | '1d' | '1w' | 'custom')[] = [
-  'permanent', '1h', '1d'
-];
